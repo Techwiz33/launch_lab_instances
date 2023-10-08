111c51f6fe63d1c875b6b7e2c2ac6c4bb4907413
aws_lab_user+1-at-243329299298 <techwiz.m33@gmail.com>
2023-10-08 10:56:19 +0800

    change image

--- ec2_jenkins.tf
+++ ec2_jenkins.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2_jenkins" {
   count = var.launch_ec2_jenkins ? 1 : 0
-  ami = "ami-083b355a397a1763c"
+  #ami = "ami-083b355a397a1763c"
   #ami = "ami-062c448666d748884"  ##  before sept 26th
-  #ami = "ami-0df8aa15ac33a9c53"
+  ami = "ami-0df8aa15ac33a9c53"
   instance_type = "t2.medium"
   key_name = var.ec2_keys
   vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
